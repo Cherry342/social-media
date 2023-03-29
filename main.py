42ed620fd755d8439e22a0e7579fdcb397ff690f
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2023-03-29 10:55:34 -0400

    add main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pymysql.cursors
 import pymysql
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -16,20 +16,40 @@ connection = pymysql.connect(
 
 @app.route('/')
 def index():
-    return render_template("home.html.jinja")
+    return render_template("feed.html.jinja")
 
 
 @app.route('/feed')
 def post_feed():
 
     cursor = connection.cursor()
-    cursor.exectute("SELECT  * FROM `posts` ORDER BY `timesstamp`")
+    cursor.execute("SELECT * FROM `post` JOIN `users` ON `post`.`user_id`= `users`.`id` ORDER BY `timestamp` DESC;")
     results = cursor.fetchall()
 
     return render_template(
         "feed.html.jinja",
         posts=results
     )
+@app.route('/sign-in')
+def sign_in():
+    return render_template ("sign_in.html.jinja")
+@app.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method=='POST':
+        #Handle signup
+        cursor= connection.cursor()
+        cursor.execute("""
+           INSERT INTO `users` (`id`, `display_name`, `password`, `banned`)
+           VALUES(%s,%s,%s, %s, %s, %s)
+        """(request))
+        return request.form
 
+    elif request.method=='GET':
+        return render_template("sign_up.html.jinja")
+
+    return render_template("sign_up.html.jinja")
+"""
+<input type="file" name= "avatar">
+"""
 if __name__ == '__main__':
     app.run(debug=True)
