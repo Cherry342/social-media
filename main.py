8aea7de90fbd631b4fbc1ffb6b778bf43cffe2aa
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2023-04-03 10:53:02 -0400

    add main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,request, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required, login_user, current_user,logout_user
 
 import pymysql
 import pymysql.cursors
@@ -7,16 +7,18 @@ login_manager= LoginManager()
 
 app = Flask(__name__)
 login_manager.init_app(app)
+app.config['SECRET_KEY'] = 'something_random'
 class User:
-   def __init__(self , id, username, banned):
+    def __init__(self , id, username, banned):
        self.is_authenticated=True
        self.is_anonymous = False
        self.is_active= not banned
 
        self.username= username
        self.id = id
-def get_id(self):
-       return str(self.id)
+
+    def get_id(self):
+        return str(self.id)
 
 
 
@@ -40,6 +42,7 @@ def user_loader(user_id):
         return None
     
     return User(result['id'], result['username'], result['banned'])
+    
 
 @app.route('/')
 def index():
@@ -47,6 +50,7 @@ def index():
 
 
 @app.route('/feed')
+@login_required
 def post_feed():
 
     cursor = connection.cursor()
@@ -57,12 +61,45 @@ def post_feed():
         "feed.html.jinja",
         posts=results
     )
-
-@app.route('/sign-in')
+@app.route('/sign_out')
+def sign_out():
+    logout_user()
+    return redirect('/sign_in')
+@app.route('/sign-in', methods=['POST', 'GET'])
 def sign_in():
-    return render_template ("sign_in.html.jinja")
+    if current_user.is_authenticated:
+        return redirect('/feed')
+    
+    if request.method =='POST':
+        cursor= connection.cursor()
+
+        cursor.execute(f"SELECT * FROM `users` WHERE `username` = '{request.form['username']} '")
+
+        result=cursor.fetchone()
+
+        if result is None:
+            return render_template("sign_in.html.jinja")
+        if request.form['password'] == result['password']:
+            user=User(result['id'], result['username'], result['banned'])
+            login_user(user)
+
+            return redirect('/feed')
+            
+        else:
+            return render_template("sign_in.html.jinja")
+        return request.form
+        
+           # do something
+    elif request.method=='GET':
+
+        return render_template ("sign_in.html.jinja")
+        
+        
+
 @app.route('/sign-up', methods=['POST', 'GET'])
 def sign_up():
+    if current_user.is_authenticated:
+        return redirect('/feed')
     if request.method=='POST':
         #Handle signup
         cursor= connection.cursor()
